2ec2da29db80270880e4ac5f4421576db0865f6f
selee <tldms296@gmail.com>
2021-09-22 18:13:42 +0900

    if/else in pug

--- src/controllers/videocontroller.js
+++ src/controllers/videocontroller.js
@@ -1,7 +1,10 @@
-export const trending = (req, res) => res.render("home", {pageTitle: "Home"});
-export const see = (req, res) => {
-    return res.render("watch");
-};
+const fakeUser = {
+    username: "selee",
+    loggedIn: false
+}
+
+export const trending = (req, res) => res.render("home", {pageTitle: "Home", fakeUser: fakeUser});
+export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
